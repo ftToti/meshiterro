17b8d7a978608037761e78e50724aa9b8b1a9b0b
ftToti <ft.1rjn@gmail.com>
2020-04-29 22:42:31 +0900

    [Finish]4-6meshiterro

---
+++ config/initializers/application_controller_renderer.rb
@@ -0,0 +1,10 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+
+Refile.secret_key = '7c19c67e3204995fc1bce7a05437ba2e83edafbfa70a851704cd7e4ab64bac8f747abe4b376fdc6d07ed9aee37ea69a7a2e78c07b9507367f6e6f1a12645ba6e'
